8fade959b1603c52d841188dda8022cb64738d8a
Amyat103 <zhoudavid7312@gmail.com>
2024-05-14 20:54:06 -0400

    added gameControl to run game

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const GameBoard = {
                 ["","",""],
                 ["","",""]],
 
+    showBoard: function() {
+        return this.gameBoard;
+    },
+
     clearBoard: function() {
         this.gameBoard = [["","",""],
                         ["","",""],
@@ -18,8 +22,8 @@ const GameBoard = {
             console.log(this.gameBoard);
             if(this.checkWin === true){
                 console.log(marker + "is the winner");
-                return true;
             }
+            return true;
         }else{
             console.log(this.gameBoard);
             return false;
@@ -60,12 +64,25 @@ const GameBoard = {
             return true;
         }
         return false;
+    },
+
+    checkDraw: function() {
+        checkFull = false;
+        for(let i = 0; i < this.gameBoard.length; i++){
+            for(let j = 0; j < this.gameBoard.length; j++){
+                if(this.gameBoard[i][j] === ""){
+                    checkFull = false;
+                }
+            }
+        }
+        checkFull = true;
+        if(checkFull === true){
+            if(this.checkWin === false){
+                return true;
+            }
+        }
+        return false;
     }
-
-
-
-
-    
     
 };
 
@@ -75,4 +92,39 @@ function Player (name, marker) {
     return {name, marker};
 }
 
+
+const gameControl = (function() {
+    let currentPlayer;
+    const player1 = Player("Player1", "X");
+    const player2 = Player("Player2", "O");
+
+    function start() {
+        currentPlayer = player1;
+        GameBoard.clearBoard();
+        console.log("gameControl.start()")
+    }
+
+    function play(row, col) {
+        // null means placed
+        if(GameBoard.placeMarker(row, col, currentPlayer.marker)){
+            console.log("placed")
+            console.log(GameBoard.showBoard());
+            this.switchPlayer();
+            if(GameBoard.checkWin === true){
+                console.log("Winner");
+                console.log(currentPlayer.name + " is the winner");
+            }
+        }else{
+            console.log("spot taken, did not place")
+        }
+    }
+
+    function switchPlayer() {
+        if(currentPlayer === player1){
+            this.currentPlayer = player2;
+        }else{
+            this.currentPlayer = player1;
+        }
+    }
+})
 
